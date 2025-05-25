d024ca9e66b8d9a9850056638985f282c15a41eb
Sergey Kudashkin <ya.sikuda@yandex.ru>
2025-05-26 01:13:22 +0300

    --step

--- devcon2024/settings.py
+++ devcon2024/settings.py
@@ -131,3 +131,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import mimetypes
+
+mimetypes.add_type("application/javascript", ".js", True)
